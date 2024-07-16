61a4d61bf52747cf7db116eccbbe4a35478c119a
Will Garvey <willmgarvey@gmail.com>
2024-07-16 18:51:56 -0500

    added backspace and letter fill during typing

--- script.js
+++ script.js
@@ -2,13 +2,57 @@ let guessCount = 1;
 
 
 
-document.getElementById('guess-input').addEventListener('keypress', function(event){
+document.getElementById('guess-input').addEventListener('input', function(event){
     let charCode = event.charCode;
     let inputValue = event.target.value;
 
     if (!/[a-zA-Z]/.test(String.fromCharCode(charCode)) || inputValue.length >= 5) {
         event.preventDefault();
     }
+
+    // Select the ID of the letter location
+    let cellSelector = `#guess-${guessCount}-${inputValue.length}`;
+    let cell = document.querySelector(cellSelector);
+
+    //check length of string to change board accordingly
+
+    if (inputValue.length > 5){
+        newInputValue = inputValue.substring(0, 5);
+        document.getElementById('guess-input').value = newInputValue;
+    }
+
+    // Null or undefined check
+    else if (inputValue != null && inputValue.length > 0) {
+        // Safe to access the last character
+        cell.innerHTML = inputValue[inputValue.length - 1];
+    } else {
+        // Handle null or empty inputValue
+        let cellSelector = `#guess-${guessCount}-1`;
+        let cell = document.querySelector(cellSelector);
+        cell.innerHTML = "";
+    }
+
+
+
+    if (inputValue.length <= 5 ||  inputValue.length > 0) {
+        inputValue[inputValue.length + 1] = "";
+
+        // Select the ID of the letter location to delete
+        let cellSelector = `#guess-${guessCount}-${inputValue.length + 1}`;
+        let cell = document.querySelector(cellSelector);
+        cell.innerHTML = "";
+    }
+
+    else {
+        // Handle null or empty inputValue
+        let cellSelector = `#guess-${guessCount}-1`;
+        let cell = document.querySelector(cellSelector);
+        cell.innerHTML = "";
+    }
+
+    console.log(inputValue.length);
+
+
 });
 
 document.getElementById('guess-button').addEventListener('click', function(event) {
